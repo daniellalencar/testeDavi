d0af5967b3d16c5f59817a456f720a133091ef5f
daniel.alencar <daniel.lima.alencar@gmail.com>
2021-07-21 22:39:46 -0300

    classe1

---
+++ src/testeDavi/ClasseDavi.java
@@ -0,0 +1,12 @@
+package testeDavi;
+
+public class ClasseDavi {
+  public static void main(String[] args) {
+    ClasseDavi classeDavi = new ClasseDavi();
+    classeDavi.escreveNome();
+  }
+
+  private void escreveNome() {
+    System.out.println("Daniel Lima ALencar");
+  }
+}
