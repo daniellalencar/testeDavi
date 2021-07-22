e86a666f3fe73d67bc52ec2fb34e896e17ba87ce
daniel.alencar <daniel.lima.alencar@gmail.com>
2021-07-21 22:43:49 -0300

    push

--- src/testeDavi/ClasseDavi.java
+++ src/testeDavi/ClasseDavi.java
@@ -4,6 +4,12 @@ public class ClasseDavi {
   public static void main(String[] args) {
     ClasseDavi classeDavi = new ClasseDavi();
     classeDavi.escreveNome();
+    classeDavi.somaValores(3, 4);
+  }
+
+  private void somaValores(int i, int j) {
+    System.out.println("Valor impresso");
+    System.out.println(i + j);
   }
 
   private void escreveNome() {
